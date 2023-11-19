2d4b7a0b9012d3457e66141ee8a024281e106688
Vladimir Kidyaev <krios2146@gmail.com>
2023-11-19 20:11:57 +0700

    chore: removing `,` & `:` from Token.toString method

--- src/main/java/krios/interpreter/Token.java
+++ src/main/java/krios/interpreter/Token.java
@@ -16,6 +16,6 @@ public class Token {
 
     @Override
     public String toString() {
-        return "Token type: " + type + ", lexeme: " + lexeme + ", literal: " + literal;
+        return "Token - type " + type + " lexeme " + lexeme + " literal " + literal;
     }
 }
