573839cba7f948b02ea9a3d7d7fe0efabd8041e5
Vladimir Kidyaev <krios2146@gmail.com>
2023-11-19 16:36:23 +0700

    feat: add `runFile` & `runPrompt` methods

---
+++ src/main/java/krios/interpreter/Lox.java
@@ -0,0 +1,44 @@
+package krios.interpreter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static java.nio.charset.Charset.defaultCharset;
+
+public class Lox {
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        }
+
+        if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] fileContent = Files.readAllBytes(Paths.get(path));
+
+        String sourceCode = new String(fileContent, defaultCharset());
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (; ; ) {
+            System.out.print("> ");
+            String line = reader.readLine();
+
+            if (line == null) {
+                break;
+            }
+        }
+    }
+}
