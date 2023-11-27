7be6219a4353f642d032891f4194b80d3bde0275
Vladimir Kidyaev <krios2146@gmail.com>
2023-11-27 22:04:56 +0700

    feat: update Parser with var declaration methods

--- src/main/java/krios/interpreter/Parser.java
+++ src/main/java/krios/interpreter/Parser.java
@@ -17,10 +17,36 @@ public class Parser {
         List<Stmt> statements = new ArrayList<>();
 
         while (!isAtEnd()) {
-            statements.add(statement());
+            statements.add(declaration());
         }
 
         return statements;
+    }
+
+    private Stmt declaration() {
+        try {
+            if (match(VAR)) {
+                return varDeclaration();
+            }
+            return statement();
+        }
+        catch (ParseError error) {
+            synchronize();
+            return null;
+        }
+    }
+
+    private Stmt varDeclaration() {
+        Token name = consume(IDENTIFIER, "Expect variable name");
+
+        Expr initializer = null;
+        if (match(EQUAL)) {
+            initializer = expression();
+        }
+
+        consume(SEMICOLON, "Expect ';' after variable declaration");
+
+        return new Stmt.Var(name, initializer);
     }
 
     private Stmt statement() {
@@ -118,6 +144,10 @@ public class Parser {
 
         if (match(NUMBER, STRING)) {
             return new Expr.Literal(previous().getLiteral());
+        }
+
+        if (match(IDENTIFIER)) {
+            return new Expr.Variable(previous());
         }
 
         if (match(LEFT_PARENTHESIS)) {
