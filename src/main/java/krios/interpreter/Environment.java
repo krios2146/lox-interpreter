06108570628bab51cc25d681688ae88dd6f267b9
Vladimir Kidyaev <krios2146@gmail.com>
2023-11-30 17:18:39 +0700

    feat: add enclosing environments to the Environment

--- src/main/java/krios/interpreter/Environment.java
+++ src/main/java/krios/interpreter/Environment.java
@@ -5,7 +5,18 @@ import java.util.Map;
 
 public class Environment {
 
-    private final Map<String, Object> values = new HashMap<>();
+    private final Environment enclosing;
+    private final Map<String, Object> values;
+
+    public Environment() {
+        this.enclosing = null;
+        this.values = new HashMap<>();
+    }
+
+    public Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+        this.values = new HashMap<>();
+    }
 
     public void define(String name, Object value) {
         values.put(name, value);
@@ -16,12 +27,21 @@ public class Environment {
             return values.get(name.getLexeme());
         }
 
+        if (enclosing != null) {
+            return enclosing.get(name);
+        }
+
         throw new RuntimeError(name, "Undefined variable '" + name.getLexeme() + "'");
     }
 
     public void assign(Token name, Object value) {
         if (values.containsKey(name.getLexeme())) {
             values.put(name.getLexeme(), value);
+            return;
+        }
+
+        if (enclosing != null) {
+            enclosing.assign(name, value);
             return;
         }
 
