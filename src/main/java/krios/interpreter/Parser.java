7b71c6a206f5d7a7054ed2c6beddd82d89ec0647
Vladimir Kidyaev <krios2146@gmail.com>
2023-11-30 17:31:18 +0700

    feat: add block to the Parser

--- src/main/java/krios/interpreter/Parser.java
+++ src/main/java/krios/interpreter/Parser.java
@@ -53,8 +53,22 @@ public class Parser {
         if (match(PRINT)) {
             return printStatement();
         }
+        if (match(LEFT_BRACE)) {
+            return new Stmt.Block(block());
+        }
 
         return expressionStatement();
+    }
+
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after block");
+        return statements;
     }
 
     private Stmt expressionStatement() {
