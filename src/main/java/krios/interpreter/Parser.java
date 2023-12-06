575bf115c90bac9377400aef269b88d15d87fada
Vladimir Kidyaev <krios2146@gmail.com>
2023-12-06 18:54:06 +0700

    feat: add for loop to the Parser

--- src/main/java/krios/interpreter/Parser.java
+++ src/main/java/krios/interpreter/Parser.java
@@ -1,6 +1,7 @@
 package krios.interpreter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static krios.interpreter.TokenType.*;
@@ -50,6 +51,9 @@ public class Parser {
     }
 
     private Stmt statement() {
+        if (match(FOR)) {
+            return forStatement();
+        }
         if (match(IF)) {
             return ifStatement();
         }
@@ -64,6 +68,53 @@ public class Parser {
         }
 
         return expressionStatement();
+    }
+
+    private Stmt forStatement() {
+        consume(LEFT_PARENTHESIS, "Expect '(' after for loop");
+
+        Stmt initializer;
+        if (match(SEMICOLON)) {
+            initializer = null;
+        } else if (match(VAR)) {
+            initializer = varDeclaration();
+        } else {
+            initializer = expressionStatement();
+        }
+
+        Expr condition = null;
+
+        if (!check(SEMICOLON)) {
+            condition = expression();
+        }
+        consume(SEMICOLON, "Expect ';' after loop condition");
+
+        Expr increment = null;
+        if (!check(RIGHT_PARENTHESIS)) {
+            increment = expression();
+        }
+        consume(RIGHT_PARENTHESIS, "Expect ')' after for clauses");
+
+        Stmt body = statement();
+
+        if (increment != null) {
+            body = new Stmt.Block(Arrays.asList(
+                    body,
+                    new Stmt.Expression(increment)
+            ));
+        }
+
+        if (condition == null) {
+            condition = new Expr.Literal(true);
+        }
+
+        body = new Stmt.While(condition, body);
+
+        if (initializer != null) {
+            body = new Stmt.Block(Arrays.asList(initializer, body));
+        }
+
+        return body;
     }
 
     private Stmt whileStatement() {
