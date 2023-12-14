518b6682033733de632384c093a2b62f31892955
krios2146 <krios2146@gmail.com>
2023-12-14 18:10:10 +0700

    feat: add the Return runtime exception

---
+++ src/main/java/krios/interpreter/Return.java
@@ -0,0 +1,15 @@
+package krios.interpreter;
+
+public class Return extends RuntimeException {
+
+    private final Object value;
+
+    public Return(Object value) {
+        super(null, null, false, false);
+        this.value = value;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
